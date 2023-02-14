a334b8d886494c126942e08106f56da8ea28e53b
Rasel <md.rasel@graaho.com>
2023-02-14 18:16:13 +0600

    Fix db json data

--- index.js
+++ index.js
@@ -97,27 +97,35 @@ server.post('/auth/login', (req, res) => {
 })
 
 server.use(/^(?!\/auth).*$/, (req, res, next) => {
-    if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-        const status = 401
-        const message = 'Error in authorization format'
-        res.status(status).json({ status, message })
-        return
-    }
-    try {
-        let verifyTokenResult;
-        verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
-
-        if (verifyTokenResult instanceof Error) {
+    if (req.method === "POST") {
+        if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
             const status = 401
-            const message = 'Access token not provided'
+            const message = 'Error in authorization format'
             res.status(status).json({ status, message })
             return
         }
+        try {
+            let verifyTokenResult;
+            verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+            if (verifyTokenResult instanceof Error) {
+                const status = 401
+                const message = 'Access token not provided'
+                res.status(status).json({ status, message })
+                return
+            }
+            next()
+        } catch (err) {
+            const status = 401
+            const message = 'Error access_token is revoked'
+            res.status(status).json({ status, message })
+        }
+    }
+    else {
+        const status = 200
+        const message = 'Success'
+        //res.status(200).json({ status, message })
         next()
-    } catch (err) {
-        const status = 401
-        const message = 'Error access_token is revoked'
-        res.status(status).json({ status, message })
     }
 })
 
